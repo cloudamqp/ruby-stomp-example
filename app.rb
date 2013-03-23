76d9f746dfffbd0ad4856c9724a01d7b1404bc90
Carl Hörberg <carl.hoerberg@gmail.com>
2013-03-23 14:22:07 +0800

    init

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'stomp'
+
+opts = { :hosts => [{:login => 'guest', :passcode => 'guest', :host => 'localhost', port: 61613 }],
+         :connect_headers => {"accept-version" => "1.1",
+                              "host" => "/" } }
+
+client = Stomp::Client.new(opts)
+
+client.publish('/queue/stomp', 'Hello world!', {:persistent => true})
+client.subscribe("/queue/stomp") do |msg|
+  puts Time.now, msg
+end
+sleep 3
